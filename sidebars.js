cadf01f0a11238669b8cadbc88842d9da12a2f90
Abhinav Sharma <abhi18av@gmail.com>
2021-02-25 09:07:01 -0300

    initial commit

---
+++ sidebars.js
@@ -0,0 +1,13 @@
+module.exports = {
+  someSidebar: {
+    Welcome: ["introduction"],
+    "Getting Started": ["deployments", "usage"],
+    Installation: ["doc1"],
+    "Compute Environments": ["doc1"],
+    "Launching Pipelines": ["doc1"],
+    "Monitoring Pipelines": ["doc1"],
+    "Git Integration": ["doc1"],
+    Automation: ["doc1"],
+    API: ["doc1"],
+  },
+};
